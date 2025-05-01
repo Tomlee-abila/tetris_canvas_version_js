784bb5438bc9fe0966f2f39d330fb688219ab354
Tomlee Abila <tomleeabila@gmail.com>
2025-05-01 11:53:51 +0300

    feat: create matrixs for different pieces

--- script.js
+++ script.js
@@ -99,6 +99,64 @@ function rotate(matrix, dir){
     }
 }
 
+function createPiece (type) {
+    if (type === 'T'){
+        return[
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ];
+    }else if (type === 'O'){
+        return[
+            [2, 2],
+            [2, 2],            
+        ];
+    }else if (type === 'L'){
+        return[
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
+        ];
+    }else if (type === 'J'){
+        return[ 
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
+        ];
+    }else if (type === 'I'){
+        return[
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+        ];
+    }else if (type === 'S'){
+        return[ 
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    }else if (type === 'Z'){
+        return[ 
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
+
+function playerReset(){
+    const pieces = 'ILJOTSZ'
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length/2 | 0) -
+                    (player.matrix[0].length/2 | 0);
+    
+    if (collide(arena, player)){
+        arena.forEach(row => row.fill(0))
+    }
+}
+
 function playerRotate(dir){
     rotate(player.matrix, dir);    
     if (collide(arena, player)){
@@ -148,7 +206,7 @@ console.table(arena);
 
 const player = {
     pos: {x: 5, y: 5},
-    matrix: matrix
+    matrix: createPiece('T')
 }
 
 document.addEventListener('keydown', event => {
